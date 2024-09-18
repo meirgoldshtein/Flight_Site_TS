90f91b037121d8fb0c4a34d15b347516e5f78610
meirgoldshtein <m0548491057@gmail.com>
2024-09-18 16:47:33 +0300

    finis all the functionality

--- app.ts
+++ app.ts
@@ -1,5 +1,5 @@
 const SELECT : HTMLSelectElement = document.querySelector("select")!;
-const BODY = document.querySelector(".body");
+const PASANGERS_CONTAINER : HTMLDivElement = document.querySelector(".body")!;
 const NAME_INPUT : HTMLInputElement = document.querySelector("#name")!;
 const MALE_BTN : HTMLInputElement = document.querySelector("#male")!;
 const FEMALE_BTN : HTMLInputElement = document.querySelector("#female")!;
@@ -26,6 +26,13 @@ interface Pasanger{
     id?: string
 }
 
+interface Details{
+    name? : string;
+    gender? : string;
+    flight_id? : string;
+    agent? : string;
+}
+
 type Data = Flight | Pasanger;
 
 
@@ -37,7 +44,7 @@ const getData = async (endPoint : string) : Promise<Data[] | Error> => {
             throw new Error(`HTTP error! status: ${response.status}`);
         }
         const data : Data[] = await response.json();
-        console.log(data);
+        // console.log(data);
         return data;
     }
     catch(err){
@@ -62,7 +69,7 @@ const postDada = async (obj : Data, endPoint : string = '') : Promise<Data | Err
             throw new Error(`HTTP error! status: ${response.status}`);
         }
         const data : Data = await response.json();
-        console.log(data);
+        // console.log(data);
         return data;
     }
     catch(err){
@@ -72,7 +79,7 @@ const postDada = async (obj : Data, endPoint : string = '') : Promise<Data | Err
 }
 
 // פונקצייה גנרית לעדכון אובייקט במסד הנתונים
-const putDada = async (obj : Data, endPoint : string = '', id : string) : Promise<Data | Error> => {
+const putDada = async <T>(obj : T, endPoint : string = '', id : string) : Promise<T | Error> => {
     try{
         const response : Response = await fetch(`${baseURL}${endPoint}/${id}`, {
             method: 'PUT',
@@ -84,7 +91,7 @@ const putDada = async (obj : Data, endPoint : string = '', id : string) : Promis
         if (!response.ok) {
             throw new Error(`HTTP error! status: ${response.status}`);
         }
-        const data : Data = await response.json();
+        const data : T = await response.json();
         console.log(data);
         return data;
     }
@@ -108,7 +115,7 @@ const deleteDada = async (endPoint : string = '', id : string) : Promise<Data | 
             throw new Error(`HTTP error! status: ${response.status}`);
         }
         const data : Data = await response.json();
-        console.log(data);
+        // console.log(data);
         return data;
     }
     catch(err){
@@ -117,14 +124,86 @@ const deleteDada = async (endPoint : string = '', id : string) : Promise<Data | 
     }
 }
 
-const dugma: Pasanger = {
-"createdAt": new Date().toISOString(),
-"name": "נחמן",
-"gender": "male",
-"flight_id": "6141d9a8b5bafeb7b438f77a",
-"agent": "טיסות לרבינו",
+// מאזין לעריכת נוסע
+const btnEditListener = (event : Event) : void => {
+    const btnEdit : HTMLButtonElement = event.target as HTMLButtonElement;
+    const card : HTMLDivElement = btnEdit.parentElement! as HTMLDivElement;
+    const inputs = card.querySelectorAll('input');
+    const btnSave = card.querySelector('.btnSave') as HTMLButtonElement;     
+    btnSave.disabled = !btnSave.disabled;
+    inputs.forEach((input: HTMLInputElement) => {
+        input.disabled = !input.disabled;  
+    });
+
 }
 
+// מאזין לעדכון פרטי נוסע
+const btnSaveListener = async (event : Event) : Promise<void>  => {
+    const btnSave : HTMLButtonElement = event.target as HTMLButtonElement;
+    const card : HTMLDivElement = btnSave.parentElement! as HTMLDivElement;
+    const inputs = card.querySelectorAll('input');
+    const name = inputs[0].value;
+    const gender = inputs[1].value;
+    const pasanger : Details = {name, gender};
+    await putDada(pasanger, 'pasangers', btnSave.id);
+    renderPassangers();
+}
+
+// פונקצייה לרנדור כל הטיסות של הסוכן הנוכחי
+const renderPassangers = async () : Promise<void | Error> => {
+    try{
+        const data : Data[] | Error = await getData('pasangers?agent=Top%20travel%20agency');
+        if(data instanceof Error){
+            console.error(data);
+            return;
+        }   
+        // console.log(data);
+        PASANGERS_CONTAINER.innerHTML = '';
+        (data as Pasanger[]).forEach((pasanger : Pasanger) => {
+            const card = document.createElement('div');
+            card.classList.add('card');
+            const details = document.createElement('div');
+            details.classList.add('details');
+            details.innerHTML = `
+                <input type="text" class="inputDetails" value="${pasanger.name}" disabled>
+                <input type="text" class="inputDetails" value="${pasanger.gender}" disabled>`;
+            card.appendChild(details);
+
+            // הוספת כפתור עריכה
+            const btnEdit : HTMLButtonElement = document.createElement('button')!;
+            btnEdit.id = pasanger.id? pasanger.id.toString(): '';
+            btnEdit.textContent = 'Edit';
+            btnEdit.classList.add('btnEdit');
+            card.appendChild(btnEdit);
+            btnEdit.addEventListener('click', (e) => btnEditListener(e));
+
+            // הוספת כפתור שמירה
+            const btnSave : HTMLButtonElement = document.createElement('button')!;
+            btnSave.textContent = 'Save';
+            btnSave.id = pasanger.id? pasanger.id.toString(): '';
+            btnSave.disabled = true;
+            btnSave.classList.add('btnSave');
+            card.appendChild(btnSave);
+            btnSave.addEventListener('click', async (e) => {btnSaveListener(e)});
+
+            // הוספת כפתור מחיקה
+            const btnDelete : HTMLButtonElement = document.createElement('button')!;
+            btnDelete.textContent = 'Delete';
+            btnDelete.classList.add('btnDelete');
+            btnDelete.id = pasanger.id? pasanger.id.toString(): '';
+            card.appendChild(btnDelete);
+            btnDelete.addEventListener('click', async (e) => {
+                await deleteDada('pasangers', btnDelete.id);
+                renderPassangers();
+            });
+            PASANGERS_CONTAINER.appendChild(card);   
+        });
+    }
+    catch(err){
+        console.error(err);
+        return err instanceof Error ? err : new Error('An unknown error occurred');
+    }
+}
 
 // פונקצייה שיוצרת אובייקט של נוסע בטיסה ומחזירה אותו
 const createPasanger =  () : Pasanger | null=>  {
@@ -147,18 +226,86 @@ const createPasanger =  () : Pasanger | null=>  {
         name: name,
         gender: gender,
         flight_id: FLIGHT_ID,
-        agent: 'טיסות לרבינו',
+        agent: AGENT_ID,
     }
 
     return pasanger;
 }
 
 
+// פונקציייה לרנדור כל הטיסות לבחירה בסלקט
+const renderFlightOptions = async () : Promise<void | Error> => {
+    
+    try{
+        const res : Data[]|Error = await getData(`flights`);
+        if(res instanceof Error){
+            console.error(res);
+            throw new Error(`${res}`);
+            
+        }
+
+        // const data : Flight[] = await response.json();
+        // console.log(res);
+        (res as Flight[]).forEach((flight : Flight) => {
+            const option = document.createElement('option') as HTMLOptionElement;
+            option.value = flight.id? flight.id : '';
+            option.textContent = flight.from + ' - ' + flight.to;
+            SELECT.appendChild(option);
+        });
+    }
+    catch(err){
+        console.error(err);
+        return err instanceof Error ? err : new Error('An unknown error occurred');
+       
+    }
+}
+
 
 SEND_BTN.addEventListener('click', async () => {
     const pasanger : Pasanger | null = createPasanger();
     if(!pasanger) return;
+    try{
+        const response : Data | Error = await  postDada(pasanger, 'pasangers');
+        if(response instanceof Error){
+            console.error(response);
+            return;
+        }
 
-    const response : Data | Error = await  postDada(pasanger, 'pasangers');
+        // console.log(response);
+        NAME_INPUT.value = '';
+    }
+    catch(err){
+        console.error(err);
+    }
+
+    
 })
 
+
+SELECT.addEventListener('change', async () => {
+    FLIGHT_ID = SELECT.value;
+    // console.log(FLIGHT_ID);
+})
+const main = async () => {
+    try {
+        await renderFlightOptions();
+        console.log('Flight options rendered successfully');  
+    } catch (error) {
+        console.error('Error rendering flight options:', error); 
+    }
+
+    try {
+        await renderPassangers();
+        console.log('Passangers rendered successfully');  
+    } catch (error) {
+        console.error('Error rendering passangers:', error); 
+    }
+};
+
+main();
+const deleteAll = async () => {
+    for(let i = 0; i < 300; i+=3){
+        await deleteDada('pasangers',i.toString());
+   }
+}
+// deleteAll();
--- js/app.js
+++ js/app.js
@@ -9,7 +9,7 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 const SELECT = document.querySelector("select");
-const BODY = document.querySelector(".body");
+const PASANGERS_CONTAINER = document.querySelector(".body");
 const NAME_INPUT = document.querySelector("#name");
 const MALE_BTN = document.querySelector("#male");
 const FEMALE_BTN = document.querySelector("#female");
@@ -26,7 +26,7 @@ const getData = (endPoint) => __awaiter(void 0, void 0, void 0, function* () {
             throw new Error(`HTTP error! status: ${response.status}`);
         }
         const data = yield response.json();
-        console.log(data);
+        // console.log(data);
         return data;
     }
     catch (err) {
@@ -48,7 +48,7 @@ const postDada = (obj_1, ...args_1) => __awaiter(void 0, [obj_1, ...args_1], voi
             throw new Error(`HTTP error! status: ${response.status}`);
         }
         const data = yield response.json();
-        console.log(data);
+        // console.log(data);
         return data;
     }
     catch (err) {
@@ -91,7 +91,7 @@ const deleteDada = (...args_1) => __awaiter(void 0, [...args_1], void 0, functio
             throw new Error(`HTTP error! status: ${response.status}`);
         }
         const data = yield response.json();
-        console.log(data);
+        // console.log(data);
         return data;
     }
     catch (err) {
@@ -99,13 +99,80 @@ const deleteDada = (...args_1) => __awaiter(void 0, [...args_1], void 0, functio
         return err instanceof Error ? err : new Error('An unknown error occurred');
     }
 });
-const dugma = {
-    "createdAt": new Date().toISOString(),
-    "name": "נחמן",
-    "gender": "male",
-    "flight_id": "6141d9a8b5bafeb7b438f77a",
-    "agent": "טיסות לרבינו",
+// מאזין לעריכת נוסע
+const btnEditListener = (event) => {
+    const btnEdit = event.target;
+    const card = btnEdit.parentElement;
+    const inputs = card.querySelectorAll('input');
+    const btnSave = card.querySelector('.btnSave');
+    btnSave.disabled = !btnSave.disabled;
+    inputs.forEach((input) => {
+        input.disabled = !input.disabled;
+    });
 };
+// מאזין לעדכון פרטי נוסע
+const btnSaveListener = (event) => __awaiter(void 0, void 0, void 0, function* () {
+    const btnSave = event.target;
+    const card = btnSave.parentElement;
+    const inputs = card.querySelectorAll('input');
+    const name = inputs[0].value;
+    const gender = inputs[1].value;
+    const pasanger = { name, gender };
+    yield putDada(pasanger, 'pasangers', btnSave.id);
+    renderPassangers();
+});
+// פונקצייה לרנדור כל הטיסות של הסוכן הנוכחי
+const renderPassangers = () => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const data = yield getData('pasangers?agent=Top%20travel%20agency');
+        if (data instanceof Error) {
+            console.error(data);
+            return;
+        }
+        // console.log(data);
+        PASANGERS_CONTAINER.innerHTML = '';
+        data.forEach((pasanger) => {
+            const card = document.createElement('div');
+            card.classList.add('card');
+            const details = document.createElement('div');
+            details.classList.add('details');
+            details.innerHTML = `
+                <input type="text" class="inputDetails" value="${pasanger.name}" disabled>
+                <input type="text" class="inputDetails" value="${pasanger.gender}" disabled>`;
+            card.appendChild(details);
+            // הוספת כפתור עריכה
+            const btnEdit = document.createElement('button');
+            btnEdit.id = pasanger.id ? pasanger.id.toString() : '';
+            btnEdit.textContent = 'Edit';
+            btnEdit.classList.add('btnEdit');
+            card.appendChild(btnEdit);
+            btnEdit.addEventListener('click', (e) => btnEditListener(e));
+            // הוספת כפתור שמירה
+            const btnSave = document.createElement('button');
+            btnSave.textContent = 'Save';
+            btnSave.id = pasanger.id ? pasanger.id.toString() : '';
+            btnSave.disabled = true;
+            btnSave.classList.add('btnSave');
+            card.appendChild(btnSave);
+            btnSave.addEventListener('click', (e) => __awaiter(void 0, void 0, void 0, function* () { btnSaveListener(e); }));
+            // הוספת כפתור מחיקה
+            const btnDelete = document.createElement('button');
+            btnDelete.textContent = 'Delete';
+            btnDelete.classList.add('btnDelete');
+            btnDelete.id = pasanger.id ? pasanger.id.toString() : '';
+            card.appendChild(btnDelete);
+            btnDelete.addEventListener('click', (e) => __awaiter(void 0, void 0, void 0, function* () {
+                yield deleteDada('pasangers', btnDelete.id);
+                renderPassangers();
+            }));
+            PASANGERS_CONTAINER.appendChild(card);
+        });
+    }
+    catch (err) {
+        console.error(err);
+        return err instanceof Error ? err : new Error('An unknown error occurred');
+    }
+});
 // פונקצייה שיוצרת אובייקט של נוסע בטיסה ומחזירה אותו
 const createPasanger = () => {
     const name = NAME_INPUT.value;
@@ -123,13 +190,73 @@ const createPasanger = () => {
         name: name,
         gender: gender,
         flight_id: FLIGHT_ID,
-        agent: 'טיסות לרבינו',
+        agent: AGENT_ID,
     };
     return pasanger;
 };
+// פונקציייה לרנדור כל הטיסות לבחירה בסלקט
+const renderFlightOptions = () => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const res = yield getData(`flights`);
+        if (res instanceof Error) {
+            console.error(res);
+            throw new Error(`${res}`);
+        }
+        // const data : Flight[] = await response.json();
+        // console.log(res);
+        res.forEach((flight) => {
+            const option = document.createElement('option');
+            option.value = flight.id ? flight.id : '';
+            option.textContent = flight.from + ' - ' + flight.to;
+            SELECT.appendChild(option);
+        });
+    }
+    catch (err) {
+        console.error(err);
+        return err instanceof Error ? err : new Error('An unknown error occurred');
+    }
+});
 SEND_BTN.addEventListener('click', () => __awaiter(void 0, void 0, void 0, function* () {
     const pasanger = createPasanger();
     if (!pasanger)
         return;
-    const response = yield postDada(pasanger, 'pasangers');
+    try {
+        const response = yield postDada(pasanger, 'pasangers');
+        if (response instanceof Error) {
+            console.error(response);
+            return;
+        }
+        // console.log(response);
+        NAME_INPUT.value = '';
+    }
+    catch (err) {
+        console.error(err);
+    }
 }));
+SELECT.addEventListener('change', () => __awaiter(void 0, void 0, void 0, function* () {
+    FLIGHT_ID = SELECT.value;
+    // console.log(FLIGHT_ID);
+}));
+const main = () => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        yield renderFlightOptions();
+        console.log('Flight options rendered successfully');
+    }
+    catch (error) {
+        console.error('Error rendering flight options:', error);
+    }
+    try {
+        yield renderPassangers();
+        console.log('Passangers rendered successfully');
+    }
+    catch (error) {
+        console.error('Error rendering passangers:', error);
+    }
+});
+main();
+const deleteAll = () => __awaiter(void 0, void 0, void 0, function* () {
+    for (let i = 0; i < 300; i += 3) {
+        yield deleteDada('pasangers', i.toString());
+    }
+});
+// deleteAll();
